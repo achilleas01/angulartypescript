e17d8e410fab9339b7b319977702fa7b96a513af
Achilleas Katsaros <a.katsaros1@aade.gr>
2021-12-07 15:57:53 +0200

    update a new reactive form

--- code-samples/Angular9/chapter10/form-samples/src/app/reactive/app.component.ts
+++ code-samples/Angular9/chapter10/form-samples/src/app/reactive/app.component.ts
@@ -1,35 +1,93 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup } from "@angular/forms";
+import { Component } from "@angular/core";
+import { AbstractControl, FormControl, FormGroup, Validators } from "@angular/forms";
+
+class Taytotita {
+  id: string;
+  description: string;
+
+
+  constructor(id: string, description: string) {
+    this.id = id;
+    this.description = description;
+  }
+
+}
+
+class Provider {
+  id: string;
+  description: string;
+
+
+  constructor(id: string, description: string) {
+    this.id = id;
+    this.description = description;
+  }
+}
 
 @Component({
-  selector: 'app-root',
-  template: `
-    <form [formGroup]="myFormModel" (ngSubmit)="onSubmit()">
-      <div>Username: <input type="text" formControlName="username"></div>
-      <div>SSN:      <input type="text" formControlName="ssn"></div>
-      <div formGroupName="passwordsGroup">
-        <div>Password:         <input type="password" formControlName="password"></div>
-        <div>Confirm password: <input type="password" formControlName="pconfirm"></div>
-      </div>
-      <button type="submit">Submit</button>
-    </form>
-  `
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  myFormModel: FormGroup;
+    myFormModel: FormGroup;
+    identityTypes = [
+      new Taytotita('1', 'ΑΤ ΑΣΤΥΝΟΜΙΚΗ ΤΑΥΤΟΤΗΤΑ'),
+      new Taytotita('2', 'ΕΣ ΕΛΛΗΝΙΚΟΣ ΣΤΡΑΤΟΣ'),
+      new Taytotita('3', 'ΠΑ ΠΟΛΕΜΙΚΗ ΑΕΡΟΠΟΡΙΑ'),
+      new Taytotita('4', 'ΛΣ ΛΙΜΕΝΙΚΟ ΣΩΜΑ')
+   ]
+    
+   mobilePhoneProviders = [
+     new Provider('1', 'COSMOTE'),
+     new Provider('2', 'VODAFONE'),
+     new Provider('3', 'WIND')
+   ]
+
 
   constructor() {
     this.myFormModel = new FormGroup({
-      username: new FormControl(''),
-      ssn     : new FormControl(''),
+      klidarithmosChoice: new FormControl("doy"),
       passwordsGroup: new FormGroup({
-        password: new FormControl(''),
-        pconfirm: new FormControl('')
-      })
+        username: new FormControl('',Validators.required),
+        password: new FormControl('', Validators.required),
+        pconfirm: new FormControl('', Validators.required)
+      }),
+      personDetails: new FormGroup({
+        afm: new FormControl(null, Validators.required),
+        lastname: new FormControl(null, Validators.required),
+        firstname: new FormControl(null, Validators.required),
+        fathername: new FormControl(null, Validators.required),
+        idKind: new FormControl(null, Validators.required),
+        identityNum: new FormControl(null),
+        ibanNum: new FormControl(null)
+      }),
+      comDetails: new FormGroup({
+        email: new FormControl('', [Validators.required, Validators.email]),
+        phoneNumber: new FormControl('', Validators.required),
+        mobilePhoneNumber: new FormControl(null),
+        mobileProvider: new FormControl(null, [Validators.required])
+      }),
+      acceptTerms: new FormControl('', Validators.required)
     });
+  }
+
+  // convenience getter for easy access to form fields
+  get f() { return this.myFormModel.controls; }
+
+  get person() {
+    return this.myFormModel.controls.personDetails as FormGroup;
+  }
+
+  get comdetails () {
+    return this.myFormModel.controls.comDetails as FormGroup;
   }
 
   onSubmit() {
     console.log(this.myFormModel.value);
+  }
+
+  changeKlType(event) {
+    console.log(event.target.value);
   }
 }
